c56d112e47e739e6be07f85c2763497f3d8dbfb5
tomasmota <tomasrebelomota@gmail.com>
2023-02-05 18:56:02 +0100

    use template
    Signed-off-by: tomasmota <tomasrebelomota@gmail.com>

--- packages/app/src/components/Root/Root.tsx
+++ packages/app/src/components/Root/Root.tsx
@@ -1,6 +1,7 @@
 import React, { PropsWithChildren } from 'react';
 import { makeStyles } from '@material-ui/core';
 import HomeIcon from '@material-ui/icons/Home';
+import CategoryIcon from '@material-ui/icons/Category';
 import ExtensionIcon from '@material-ui/icons/Extension';
 import MapIcon from '@material-ui/icons/MyLocation';
 import LibraryBooks from '@material-ui/icons/LibraryBooks';
@@ -66,11 +67,12 @@ export const Root = ({ children }: PropsWithChildren<{}>) => (
       <SidebarDivider />
       <SidebarGroup label="Menu" icon={<MenuIcon />}>
         {/* Global nav, not org-specific */}
-        <SidebarItem icon={HomeIcon} to="catalog" text="Home" />
+        <SidebarItem icon={HomeIcon} to="/" text="Home" />
+        <SidebarItem icon={CategoryIcon} to="catalog" text="Catalog" />
         <SidebarItem icon={ExtensionIcon} to="api-docs" text="APIs" />
         <SidebarItem icon={LibraryBooks} to="docs" text="Docs" />
-        <SidebarItem icon={CreateComponentIcon} to="create" text="Create..." />
         <SidebarItem icon={LayersIcon} to="explore" text="Explore" />
+        <SidebarItem icon={CreateComponentIcon} to="create" text="Create..." />
         {/* End global nav */}
       </SidebarGroup>
       <SidebarSpace />
--- packages/app/src/components/home/HomePage.tsx
+++ packages/app/src/components/home/HomePage.tsx
@@ -1,5 +1,105 @@
+import { Content, Header, Page } from '@backstage/core-components';
+import {
+  ClockConfig,
+  ComponentAccordion,
+  ComponentTab,
+  ComponentTabs,
+  HeaderWorldClock,
+  HomePageRandomJoke,
+  HomePageStarredEntities,
+  WelcomeTitle,
+} from '@backstage/plugin-home';
+import { HomePageSearchBar } from '@backstage/plugin-search';
+import { Grid } from '@material-ui/core';
 import React from 'react';
 
-export const HomePage = () => {
-  return <h1> Hello World! </h1>
+const clockConfigs: ClockConfig[] = [
+  {
+    label: 'NYC',
+    timeZone: 'America/New_York',
+  },
+  {
+    label: 'UTC',
+    timeZone: 'UTC',
+  },
+  {
+    label: 'STO',
+    timeZone: 'Europe/Stockholm',
+  },
+  {
+    label: 'TYO',
+    timeZone: 'Asia/Tokyo',
+  },
+];
+
+const timeFormat: Intl.DateTimeFormatOptions = {
+  hour: '2-digit',
+  minute: '2-digit',
+  hour12: false,
 };
+export const HomePage = () => (
+  <Page themeId="home">
+    <Header title={<WelcomeTitle />} pageTitleOverride="Home">
+      <HeaderWorldClock
+        clockConfigs={clockConfigs}
+        customTimeFormat={timeFormat}
+      />
+    </Header>
+    <Content>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <HomePageSearchBar />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <HomePageRandomJoke />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <HomePageRandomJoke
+            defaultCategory="any"
+            Renderer={ComponentAccordion}
+          />
+          <HomePageRandomJoke
+            title="Another Random Joke"
+            Renderer={ComponentAccordion}
+          />
+          <HomePageRandomJoke
+            title="One More Random Joke"
+            defaultCategory="programming"
+            Renderer={ComponentAccordion}
+          />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <ComponentTabs
+            title="Random Jokes"
+            tabs={[
+              {
+                label: 'Programming',
+                Component: () => (
+                  <HomePageRandomJoke
+                    defaultCategory="programming"
+                    Renderer={ComponentTab}
+                  />
+                ),
+              },
+              {
+                label: 'Any',
+                Component: () => (
+                  <HomePageRandomJoke
+                    defaultCategory="any"
+                    Renderer={ComponentTab}
+                  />
+                ),
+              },
+            ]}
+          />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <HomePageStarredEntities />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <HomePageStarredEntities />
+        </Grid>
+      </Grid>
+    </Content>
+  </Page>
+);
